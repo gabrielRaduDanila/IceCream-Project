5a7fe0e5eed41f5695209c74f29f83e61ba465b1
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-02-26 21:18:03 +0200

    finish product section

--- js/toggleModals.js
+++ js/toggleModals.js
@@ -1,3 +1,7 @@
 function toggleModals(id) {
   document.getElementById(id).classList.toggle('hidden-modal');
 }
+
+function showOverlay(id){
+  document.getElementById(id).classList.toggle('show-produc-info');
+}
