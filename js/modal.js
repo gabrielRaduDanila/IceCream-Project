7066a8cbf445bdc621236bffadf57c28f8f6c9f7
AlexandruMoldoveanu95 <111571285+AlexandruMoldoveanu95@users.noreply.github.com>
2023-02-26 08:01:38 +0200

    ghghghg

--- js/modal.js
+++ js/modal.js
@@ -22,14 +22,3 @@ function toggleModal(id) {
 // }// function showModal() {
 //   document.getElementById('contact-modal').classList.add('hide-modal');
 // }
-
-document.querySelector(".open-modal").addEventListener("click", function () {
-  document.querySelector("[data-modal]").classList.toggle("is-hidden");
-  var data = `
-    <div>
-      <p> test </p>
-      <h1> test 2 </h1>
-    </div>
-  `;
-  document.querySelector(".modal-paragraph").innerHTML = data;
-});
