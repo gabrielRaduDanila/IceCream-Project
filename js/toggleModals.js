dcb8795365e7bb508cf3f6ec11ae5b886d7a33af
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-02-27 00:08:59 +0200

    add mobile menu
    I have added all the functionality on buttons for mobile menu

--- js/toggleModals.js
+++ js/toggleModals.js
@@ -5,3 +5,16 @@ function toggleModals(id) {
 function showOverlay(id){
   document.getElementById(id).classList.toggle('show-produc-info');
 }
+
+const mobileMenuBtns = document.querySelectorAll('.mobile-menu__item');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuBtns.forEach((item) => item.addEventListener("click",function(){
+  mobileMenu.classList.remove("is-open")
+}))
+
+function buyNowBtn(){
+  const modal = document.querySelector("[data-modal]");
+  mobileMenu.classList.remove('is-open');
+  modal.classList.toggle('is-hidden');
+}
